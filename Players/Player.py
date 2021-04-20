0c7327db8851cc348f98bfbc26cb8988d1f79244
nmarotte <nathan.marotte@ulb.be>
2021-04-20 19:59:46 +0200

    renamed flag and changed type of return of choose_action

--- Players/Player.py
+++ Players/Player.py
@@ -18,15 +18,17 @@ class Player:
 
     def play_against(self, opponent: Player, game: Connect4Game):
         my_turn = False
+        yellow_goes_first = game.get_turn() == 1
         winner = game.get_win()
         while winner is None:
             if my_turn:
                 game.place(self.choose_action(game))
             else:
-                game.place(opponent.choose_action(game)[0])
+                game.place(opponent.choose_action(game))
 
             winner = game.get_win()
+            my_turn = not my_turn
 
-        # if winner != 0 and game.get_turn() == 2:
-        #     winner = 3 - winner
+        if winner != 0 and not yellow_goes_first:
+            winner = 3 - winner
         return winner
